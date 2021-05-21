5c3a0bb65dbb8019cafa4805435719e8667fc7c0
Robert Heath <robert.b.heathii@gmail.com>
2021-05-21 14:50:33 -0500

    Added to make git happy. Otherwise no changes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 describe User do
-  before :each do
-    user = User.new(first_name: 'Robert', last_name: 'Heath', email: 'robert.b.heathii@gmail.com', phone_number: 3049054977, street_address: '674 Locust', city: 'Chester', state: 'WV', zip: 26034)
-  end
-
   describe 'validations' do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
